52f5c8a15521fc7d9e20456dcec9c77bc0185a1a
sanchita rani podder <sanchita@sanchitas-mbp.lan>
2020-01-31 19:59:43 -0500

    sanchita project

--- maven_demo2/src/main/java/browserOption/BrowserConfig.java
+++ maven_demo2/src/main/java/browserOption/BrowserConfig.java
@@ -1,5 +1,17 @@
 package browserOption;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class BrowserConfig {
+	public static Properties readProp(String path) throws IOException {
+		File fi=new File(path);
+		FileInputStream f=new FileInputStream(fi);
+		Properties prop=new Properties();
+		prop.load(f);
+		return prop;
+	}
 
 }
